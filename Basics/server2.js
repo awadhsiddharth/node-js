22b85f1d5f5483a08da7caceedebbfdb9e595e40
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-16 06:48:04 +0000

    server done

--- Basics/server2.js
+++ Basics/server2.js
@@ -3,6 +3,7 @@ var dateTime = require('./module')
 
 //create a server object:
 http.createServer(function (req, res) {
+  res.writeHead(200,{"Content-Type" : "text/html"});
   res.write('Hello World!'); //write a response to the client
   res.write("The current date and time are : " +dateTime.myDateAndTime())
   res.end(); //end the response
