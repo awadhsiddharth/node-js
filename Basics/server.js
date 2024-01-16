5b64d9dd4bb53af272391c97d1eb806d564025ae
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-16 05:19:27 +0000

    server done

--- Basics/server.js
+++ Basics/server.js
@@ -1,6 +1,6 @@
 var http = require('http')
 
 http.createServer( (req, res)=>{
-    res.writeHead(200, {'Content-Type': 'text/plain});
-    res.end("Hey There!")
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end("Hey There!");
 }).listen(8080);
