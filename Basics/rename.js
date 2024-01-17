bb5135aeb64be8d31c3c003caa06647d5c12b5c3
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-17 06:16:48 +0000

    renam done

--- Basics/rename.js
+++ Basics/rename.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 
+// To rename a file with the File System module,  use the fs.rename() method.
 fs.rename('mynewfile1.txt', 'myrenamedfile.txt', function (err) {
   if (err){ 
     throw err;
