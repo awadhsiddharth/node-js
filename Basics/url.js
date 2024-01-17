0ca47665e42de4c596ad12340d9677567e07f035
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-17 09:27:39 +0000

    url done

---
+++ Basics/url.js
@@ -0,0 +1,13 @@
+// to include the url module, use the require() method
+
+var url = require("url")
+
+var add = 'http;//localhost:8080/default.htm?year=2017&month=february'
+var q = url.parse(add,true)
+
+console.log(q.host);    // returns 'localhost:8080'
+console.log(q.pathname);  //returns '/default.htm'
+console.log(q.search);   // returns '?year=2017&month=february'
+
+var qdata = q.query;  // returns an object: {year: 2017, month:'february'}
+console.log(qdata.month);   // returns 'february'
