18777465d3921aa665a7b33c7ddc7da256bc0dfa
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-17 11:46:52 +0000

    events done

--- Basics/event.js
+++ Basics/event.js
@@ -1,6 +1,10 @@
 // event- every action on computer is an event
 // like a connection is made or a file is opened
 
+// object in node can fire events, like readStream 
+// object fires events when opening and closing a file
+
+
 var fs = require('fs')
 var rs = fs.createReadStream('./demofile.txt')
 rs.on('open',()=>{
