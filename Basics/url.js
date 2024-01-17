c3a3bb8d698bf49b9b52baacc20e164f536d4ba3
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-17 09:28:48 +0000

    url done

--- Basics/url.js
+++ Basics/url.js
@@ -3,6 +3,8 @@
 var url = require("url")
 
 var add = 'http;//localhost:8080/default.htm?year=2017&month=february'
+
+// parse an address with url.parse() , it will return a URL obj with each part of address as properties
 var q = url.parse(add,true)
 
 console.log(q.host);    // returns 'localhost:8080'
