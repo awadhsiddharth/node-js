fe758cffb34ff2e5e14c835f3964630848fd618f
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-19 09:01:04 +0000

    select done

--- Events/select.js
+++ Events/select.js
@@ -1,19 +1,68 @@
+// var mysql = require('mysql')
+
+// var con = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'username',
+//     password: 'password',
+//     database: "mydb"
+// });
+
+// select all
+// con.connect((err) => {
+//     if (err) {
+//         throw err;
+//         con.query("SELECT * FROM customers", (err, result, fields) => {
+//             if (err) {
+//                 throw err;
+//                 console.log(result);
+//             }
+//         });
+//     }
+// });
+
+
+// selecting columns
+
 var mysql = require('mysql')
 
 var con = mysql.createConnection({
     host: 'localhost',
     user: 'username',
     password: 'password',
-    database: "mydb"
+    database: 'mydb'
 });
 
-con.connect((err) => {
+con.connect((error) => {
     if (err) {
         throw err;
-        con.query("SELECT * FROM customers", (err, result, fields) => {
-            if (err) {
+        con.query("SELECT name, address FROM cutomers", (error, result, fields) => {
+            if (error) {
                 throw err;
                 console.log(result);
+            }
+        });
+    }
+});
+
+
+// selecting fields object
+
+var mysql = require('mysql')
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'username',
+    password: 'password',
+    database: 'mydb'
+});
+
+con.connect((error) => {
+    if (err) {
+        throw err;
+        con.query("SELECT name, address FROM cutomers", (error, result, fields) => {
+            if (error) {
+                throw err;
+                console.log(fields);
             }
         });
     }
