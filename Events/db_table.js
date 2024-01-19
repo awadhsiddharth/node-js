87c29f700866cc45dbd532e6f1820f58d5ffc827
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-19 06:54:28 +0000

    table done

--- Events/db_table.js
+++ Events/db_table.js
@@ -7,15 +7,29 @@ var con = mysql.createConnection({
     database: 'mydb'
 });
 
+// con.conn((err) => {
+//     if (err) {
+//         throw err;
+//         console.log("Connected!");
+//         var sql = "CREATE TABLE customers (id INT, name VARCHAR(255),address VARCHAR(255))";
+//         con.query(sql, (err, result) => {
+//             if (err) {
+//                 throw err;
+//                 console.log("Table Created!!");
+//             }
+//         });
+//     }
+// });
+
 con.conn((err) => {
     if (err) {
         throw err;
         console.log("Connected!");
-        var sql = "CREATE TABLE customers (id int, name VARCHAR(255),address VARCHAR(255))";
+        var sql = "CREATE TABLE customers (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(255),address VARCHAR(255))";
         con.query(sql, (err, result) => {
             if (err) {
                 throw err;
-                console.log("Table Created!!");
+                console.log("Table Created with ID as primary key!!");
             }
         });
     }
