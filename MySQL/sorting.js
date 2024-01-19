cf3a7a7c7dd04ee77dc32e6394f82b606a4fae00
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-19 09:21:59 +0000

    sort done

--- MySQL/sorting.js
+++ MySQL/sorting.js
@@ -18,3 +18,24 @@ con.connect((err) => {
         });
     }
 });
+
+
+
+// Order by desc
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "yourusername",
+    password: "yourpassword",
+    database: "mydb"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    con.query("SELECT * FROM customers ORDER BY name DESC", function (err, result) {
+        if (err) throw err;
+        console.log(result);
+    });
+});
