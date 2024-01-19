d2621e86942b3f1ecd3b00664768f4a79669b51a
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-01-19 09:20:24 +0000

    sorting done

---
+++ MySQL/sorting.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql')
+
+var con = mysql.createConnection({
+    host: 'localhost',
+    user: 'username',
+    password: 'password',
+    database: 'mydb'
+});
+
+con.connect((err) => {
+    if (err) {
+        throw err;
+        con.query("SELECT * FROM customers ORDER BY name", (err, result) => {
+            if (err) {
+                throw err
+                console.log(result);
+            }
+        })
+    }
+});
