3c37f859b3cfbf44cc544661d3b86f9dbed381db
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-05 11:25:15 +0000

    second done

--- Node/second.js
+++ Node/second.js
@@ -1,7 +1,12 @@
+// module rapper function
+// (function(exports, require, module,__filename, __dirname){
+
 sid = {
     name:"Siddharth",
     favNum:18,
     developer:false
 }
-
+console.log(exports, require, module,__filename, __dirname)
 module.exports = sid;
+
+// })
