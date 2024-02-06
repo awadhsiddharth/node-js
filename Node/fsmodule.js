0067b05e3cd81fca8ac41caed8b20cdee5798ba4
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-06 05:51:29 +0000

    node changes done

--- Node/fsmodule.js
+++ Node/fsmodule.js
@@ -1,7 +1,23 @@
 const fs = require('fs')
 
-fs.readFile('js.log','utf8',(err,data)=>{
-    console.log(err,data);
-})
+// Non blocking IO model
+// fs.readFile('js.log','utf8',(err,data)=>{
+//     console.log(err,data);
+// })
+
+// console.log("Finished reading file");
+
+// const a = fs.readFileSync('js.log')
+// console.log(a);
+// to read content of file,use toString() method
+// console.log(a.toString());
+
+// fs.writeFile("file2.txt","This is a data",()=>{
+//     console.log("Written to the file");
+// });
+
+const b = fs.writeFileSync('file2.txt',"This is a data2");
+console.log(b);
+
 
 console.log("Finished reading file");
