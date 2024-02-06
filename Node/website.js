7eef7fd500d377e1c9e321cc08c290a242e72e4e
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-06 09:05:46 +0000

    website done

--- Node/website.js
+++ Node/website.js
@@ -1,11 +1,12 @@
 const http = require('http')
+const fs = require('fs')
 
 const port = process.env.PORT || 3000;
 
 
 const server = http.createServer((req, res) => {
     // console.log(req);
-  
+
     res.setHeader('Content-Type', 'text/html')
     console.log(req.url);
 
@@ -17,7 +18,12 @@ const server = http.createServer((req, res) => {
         res.statusCode = 200;
         res.end('<h1>About Rahul</h1> <p> Hey! Welcome to coding ki nayi duniya</p>');
     }
-    else{
+    else if (req.url == '/hello') {
+        res.statusCode = 200;
+        const data = fs.readFileSync('Node/index.html')
+        res.end(data.toString());
+    }   
+    else {
         // res.rahul();
         res.statusCode = 404
         res.end('<h1>Not Found!</h1> <p> Hey! This page was not found on the server</p>');
