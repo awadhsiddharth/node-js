40678df16f8e02def84f70a531b084f91f4616ba
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-06 06:32:12 +0000

    url done

---
+++ Events/seven.js
@@ -0,0 +1,16 @@
+const EventEmitter = require('events');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+
+myEmitter.on('WaterFull', () => {
+  console.log('Please turn off the motor!');
+    setTimeout(()=>{
+        console.log(`Please turn off the motor! It's gentle reminder`);
+    },3000)
+});
+
+console.log("The script is running");
+console.log("The script is still running");
+myEmitter.emit('WaterFull');
