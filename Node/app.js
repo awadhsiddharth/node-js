101b8369fb8dbdef09a09c9529434bc7ef0b0db2
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-09 09:26:37 +0000

    app done

---
+++ Node/app.js
@@ -0,0 +1,25 @@
+// const path = require('path');
+
+// var pathObject = path.parse(__filename);
+// console.log(pathObject);
+
+// const os = require('os');
+// const { uptime } = require('process');
+
+// var totalMemory = os.totalmem();
+// var freeMemory = os.freemem();
+// var upTime = os.uptime();
+
+// console.log('Total Memory : ' +totalMemory);
+// console.log('Free Memory : ' +freeMemory);
+
+// Template String
+// ES6(ES2015) :ECMAScript 6
+
+// console.log(`Total Memory : ${totalMemory}`);
+// console.log(`Free Memory : ${freeMemory}`);
+// console.log(`Up Time is : ${upTime}`);
+
+
+
+
