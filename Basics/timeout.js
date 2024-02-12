1325cb9f94b5895516178cceb90c0b5801cd3a06
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-12 06:10:39 +0000

    timeout

--- Basics/timeout.js
+++ Basics/timeout.js
@@ -10,6 +10,20 @@
 
 // console.log("Hey There!");
 
-setInterval(()=>{
-    console.log("Runs after every 2 seconds");
-},2000);
+// setInterval(()=>{
+//     console.log("Runs after every 2 seconds");
+// },2000);
+
+console.log("Hello --> Number 1");
+
+setImmediate(()=>{
+    console.log('Running before timeout --> Number 3');
+});
+
+setTimeout(()=>{
+    console.log('The timeout running last --> Number 4');
+},5000);
+
+process.nextTick(()=>{
+    console.log('Running at next tick --> Number 2');
+})
