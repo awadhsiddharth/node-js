2627e599c4bdb2264f060ed18c8f784472c6a071
Siddharth Awadh <96815992+awadhsiddharth@users.noreply.github.com>
2024-02-12 05:57:50 +0000

    timeout

--- Basics/timeout.js
+++ Basics/timeout.js
@@ -1,11 +1,15 @@
 // while writing js code, to delay execution of fn use setTimeout
 
-const a = setTimeout(()=>{
-    console.log("Setting timeout of 5 seconds.");
-    function printName(name) {
-        var a = prompt("Enter Your Name: ");
-        console.log(a);
-    }
-},5000);
+// const a = setTimeout(()=>{
+//     console.log("Setting timeout of 5 seconds.");
+//     function printName(name) {
+//         var a = prompt("Enter Your Name: ");
+//         console.log(a);
+//     }
+// },5000);
 
-console.log("Hey There!");
+// console.log("Hey There!");
+
+setInterval(()=>{
+    console.log("Runs after every 2 seconds");
+},2000);
